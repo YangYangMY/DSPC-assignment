d844f294a02c29bc80d59c08a3da07ada2f4bb43
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-09-11 22:25:40 +0800

    Updated Runable Code

---
+++ RemoveBackground(Assy).py
@@ -0,0 +1,48 @@
+import torch
+from pathlib import Path
+from rembg import remove, new_session
+from PIL import Image
+import io
+import time
+
+if torch.cuda.is_available():
+    print("GPU is available")
+else:
+    print("Using CPU")
+
+
+def ProcessImage():
+    input_dir = Path('tests/fixtures')
+    input_files = list(input_dir.glob('*.jpg'))
+    output_dir = input_dir / 'output'
+    session = new_session()
+
+    start_time = time.time()  # Record start time
+
+    for input_file in input_files:
+        try:
+            input_path = str(input_file)
+            output_path = str(output_dir / (input_file.stem + ".out.png"))
+
+            with open(input_path, 'rb') as i:
+                input_data = i.read()
+
+                output_data = remove(input_data, session=session)
+                output_image = Image.open(io.BytesIO(output_data))  # Convert bytes to PIL Image
+                print('saved : ', output_path)
+                output_image.save(output_path)
+
+        except Exception as e:
+            print(f"Error processing {input_file}: {e}")
+
+    end_time = time.time()  # Record end time
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.2f} seconds")
+
+
+def main():
+    ProcessImage()
+
+
+if __name__ == "__main__":
+    main()
