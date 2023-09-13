4f2c0a595e584bdd39ba50c247e7e3fb79a9d3c1
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-09-13 22:15:09 +0800

    Added Automatically delete output Image

--- RemoveBackground(Assy).py
+++ RemoveBackground(Assy).py
@@ -4,6 +4,7 @@ from rembg import remove, new_session
 from PIL import Image
 import io
 import time
+import os
 
 if torch.cuda.is_available():
     print("GPU is available")
@@ -11,10 +12,22 @@ else:
     print("Using CPU")
 
 
+def clean_output_directory(output_dir):
+    for file_name in os.listdir(output_dir):
+        if file_name.endswith('.png'):
+            file_path = os.path.join(output_dir, file_name)
+            os.remove(file_path)
+            print(f"Deleted {file_path}")
+
+
 def ProcessImage():
     input_dir = Path('tests/fixtures')
     input_files = list(input_dir.glob('*.jpg'))
     output_dir = input_dir / 'output'
+
+    # Clean the output directory before processing
+    clean_output_directory(output_dir)
+
     session = new_session()
 
     start_time = time.time()  # Record start time
