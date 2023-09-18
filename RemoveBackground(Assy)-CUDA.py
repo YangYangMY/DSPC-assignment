c3dc80dcebcf7b1ee715daf1d9facf624f3ebdaf
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-09-18 12:24:07 +0800

    Removed CPU usage time in CUDA File

--- RemoveBackground(Assy)-CUDA.py
+++ RemoveBackground(Assy)-CUDA.py
@@ -22,17 +22,12 @@ def ProcessImage(use_gpu):
     clean_output_directory(output_dir)
 
     gpu_execution_time = 0.0  # Initialize GPU execution time
-    cpu_execution_time = 0.0  # Initialize CPU execution time
 
     if use_gpu and torch.cuda.is_available():
         print("Using GPU")
         torch.cuda.synchronize()  # Ensure that previous GPU operations are finished
         # Set the CUDA execution provider explicitly
         session = new_session(execution_providers=['CUDAExecutionProvider'])
-    else:
-        print("Using CPU")
-        # Set the CUDA execution provider explicitly
-        session = new_session(execution_providers=['CPUExecutionProvider'])
 
     for input_file in input_files:
         try:
@@ -51,28 +46,15 @@ def ProcessImage(use_gpu):
                     torch.cuda.synchronize()  # Ensure that all GPU operations are finished
                     gpu_end_time = time.time()  # Record GPU end time
                     gpu_execution_time += (gpu_end_time - gpu_start_time)
-                else:
-                    cpu_start_time = time.time()  # Record CPU start time
-                    output_data = remove(input_data, session=session)
-                    output_image = Image.open(io.BytesIO(output_data))  # Convert bytes to PIL Image
-                    print('saved : ', output_path)
-                    output_image.save(output_path)
-                    cpu_end_time = time.time()  # Record CPU end time
-                    cpu_execution_time += (cpu_end_time - cpu_start_time)  # Measuring CPU time here
 
         except Exception as e:
             print(f"Error processing {input_file}: {e}")
 
     if use_gpu and torch.cuda.is_available():
         print(f"GPU Execution time: {gpu_execution_time:.2f} seconds")
-    else:
-        print(f"CPU Execution time (without GPU): {cpu_execution_time:.2f} seconds")
 
 def main():
-    # Run the code first with CPU
-    ProcessImage(use_gpu=False)
-
-    # Then run the code with GPU
+    # Run Code with GPU
     ProcessImage(use_gpu=True)
 
 if __name__ == "__main__":
