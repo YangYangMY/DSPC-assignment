592a1ad5f31c165e1e4ebb7459e238f65c5537fb
Lee Wee Yan <69131215+YangYangMY@users.noreply.github.com>
2023-09-22 12:30:33 +0800

    Update RemoveBackground(Assy)-Multithreading.py

--- RemoveBackground(Assy)-Multithreading.py
+++ RemoveBackground(Assy)-Multithreading.py
@@ -7,6 +7,8 @@ import io
 import time
 import os
 
+images_per_thread = 1
+
 if torch.cuda.is_available():
     print("GPU is available")
 else:
@@ -25,17 +27,21 @@ def remove_background(input_path, output_path, session):
             input_data = i.read()
             output_data = rembg.remove(input_data, session=session)
             output_image = Image.open(io.BytesIO(output_data))  # Convert bytes to PIL Image
-            print('saved : ', output_path)
+            # print('saved : ', output_path)
             output_image.save(output_path)
     except Exception as e:
             print(f"Error processing {input_path}: {e}")
 
-def process_image_in_thread(input_file, output_dir, session):
-    output_path = str(output_dir / (input_file.stem + ".out.png"))
-    remove_background(str(input_file), output_path, session)
+def process_image_in_thread(input_files, output_dir, session, start_index, end_index):
+    current_thread = threading.current_thread()
+    for i in range(start_index, end_index):
+        input_file = input_files[i]
+        output_path = str(output_dir / (input_file.stem + f"_{current_thread.name}.out.png"))
+        print(f"Process in thread {current_thread.name}: {input_file}")
+        remove_background(str(input_file), output_path, session)
 
 def main():
-    input_dir = Path('tests/fixtures')
+    input_dir = Path('100KB')
     input_files = list(input_dir.glob('*.jpg'))
     output_dir = input_dir / 'output'
 
@@ -48,12 +54,18 @@ def main():
     # Record start time
     start_time = time.time()
 
-    # One thread per input file
-    num_threads = len(input_files)
+    num_threads = 8
     threads = []
 
-    for input_file in input_files[:num_threads]:
-        thread = threading.Thread(target=process_image_in_thread, args=(input_file, output_dir, session))
+    # Split input files into equal parts for each threads
+    batch_size = len(input_files) // num_threads
+    # input_files_batches = [input_files[i:i + batch_size] for i in range(0, len(input_files), batch_size)]
+
+    for i in range(num_threads):
+        # Executing same function but input files are splited into different batches and run in the created threads
+        start_index = i * batch_size
+        end_index = min((i + 1) * batch_size, len(input_files))
+        thread = threading.Thread(target=process_image_in_thread, args=(input_files, output_dir, session, start_index, end_index))
         threads.append(thread)
         thread.start()
 
